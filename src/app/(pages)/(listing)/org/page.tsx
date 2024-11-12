ddd33fc190b27ce4b88c3d69c381cb2cdcc88636
Raiwin Inthasit <andaraiwin@gmail.com>
2024-11-12 13:35:45 +0700

    add init org page

--- src/app/(pages)/(listing)/org/page.tsx
+++ src/app/(pages)/(listing)/org/page.tsx
@@ -2,10 +2,21 @@ import OrgCarousel from "@/components/carousels/OrgCarousel";
 import React from "react";
 
 export default function OrgPage() {
-  return  <div className="font-prompt max-w-[1170px] mx-auto px-6 pb-5">
-  this is econ org page
-  <div className="flex justify-between mt-4 ">
-      <OrgCarousel />
-  </div>
-</div>;
+  return  (
+  <div className="font-prompt max-w-[1170px] mx-auto px-6 pb-5">
+    <div className="text-center font-semibold text-2xl border-b-2 pt-[25px] pb-[11px]">
+      <span className="text-black-normal">ค้นหา</span>
+      <span className="text-orange-normal"> &quot;องค์กร&quot; </span>
+      <span className="text-black-normal">ที่ตอบโจทย์เป้าหมายของคุณ</span>
+      {/* <span className="text-gray-400 text-sm ml-2">({`จำนวนองค์กรทั้งหมด`})</span> */}
+    </div>
+
+    <div className="mt-8">
+      <p className="font-medium text-2xl">งานที่เปิดรับสมัคร</p>
+    </div>
+
+    <div className="flex justify-between mt-4 ">
+        <OrgCarousel />
+    </div>
+</div>);
 }
