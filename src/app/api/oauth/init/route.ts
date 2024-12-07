87373b372839ee018864e25269f9e00baff353fd
Wuttipat Sanchai <wuttipat.gamxu@gmail.com>
2024-12-08 03:17:59 +0700

    use format url on redirect

--- src/app/api/oauth/init/route.ts
+++ src/app/api/oauth/init/route.ts
@@ -1,7 +1,8 @@
+import { formatExternalUrl } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const GOLANG_AUTH_URL = process.env.GO_API_URL + "/auth/google";
+  const GOLANG_AUTH_URL = formatExternalUrl("/auth/google")
 
   return NextResponse.redirect(GOLANG_AUTH_URL);
 }
