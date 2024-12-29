7a8caf3605c2ad96e1f685ebe09976e1a55a8627
Wuttipat Sanchai <wuttipat.gamxu@gmail.com>
2024-12-29 20:39:49 +0700

    update test case

--- __test__/JobCard.test.tsx
+++ __test__/JobCard.test.tsx
@@ -5,36 +5,60 @@ describe("JobCard", () => {
   test("renders JobCard component", () => {
     render(
       <JobCard
-        orgName="องค์กรทดสอบ 5"
-        imgUrl="https://drive.google.com/uc?export=view&id=1mzjpHi5GHFrUEEmI_EVLfQE9ht2--ILd"
-        jobTitle="งานทดสอบ 5"
-        location="ออนไลน์"
+        orgName="องค์กรทดสอบ 1"
+        title="พนักงานทดสอบ 1"
+        description="สร้างเว็บไซต์ให้บริษัททดสอบ 1 ของคุณ"
+        work_type="Fulltime"
+        workplace="Hybrid"
+        career_stage="Entry-Level"
+        province="กรุงเทพมหานคร"
+        country="ประเทศไทย"
+        salary="30,000"
+        imgUrl="https://drive.google.com/uc?export=view&id=1bsT5WNkFnhhGT7SD3AynO9gqDjzz17lc"
+        updatedDate="2024-12-29:10:00"
+        industry={["IT", "เทคโนโลยี", "องค์กร", "งานทดสอบ"]}
       />
     ); // ARRANGE
-    const element = screen.getByText(/องค์กรทดสอบ 5/i); // ACT
+    const element = screen.getByText(/พนักงานทดสอบ 1/i); // ACT
     expect(element).toBeInTheDocument(); // ASSERT
   });
 
   test("renders JobCard component with different props", () => {
     render(
       <JobCard
-        orgName="องค์กรทดสอบ 4"
-        imgUrl="https://drive.google.com/uc?export=view&id=1D9ldIaOqNZVaGemuZiKPbHHZOgAv46S9"
-        jobTitle="งานทดสอบ 4"
-        location="ออนไลน์"
+        orgName="องค์กรทดสอบ 1"
+        title="พนักงานทดสอบ 1"
+        description="สร้างเว็บไซต์ให้บริษัททดสอบ 1 ของคุณ"
+        work_type="Fulltime"
+        workplace="Hybrid"
+        career_stage="Entry-Level"
+        province="กรุงเทพมหานคร"
+        country="ประเทศไทย"
+        salary="30,000"
+        imgUrl="https://drive.google.com/uc?export=view&id=1bsT5WNkFnhhGT7SD3AynO9gqDjzz17lc"
+        updatedDate="2024-12-29:10:00"
+        industry={["IT", "เทคโนโลยี", "องค์กร", "งานทดสอบ"]}
       />
     );
-    const element = screen.getByText(/องค์กรทดสอบ 4/i);
+    const element = screen.getByText(/พนักงานทดสอบ 1/i);
     expect(element).toBeInTheDocument();
   });
 
   test("check if the image is rendered", () => {
     render(
       <JobCard
-        orgName="องค์กรทดสอบ 4"
-        imgUrl="https://drive.google.com/uc?export=view&id=1D9ldIaOqNZVaGemuZiKPbHHZOgAv46S9"
-        jobTitle="งานทดสอบ 4"
-        location="ออนไลน์"
+        orgName="องค์กรทดสอบ 1"
+        title="พนักงานทดสอบ 1"
+        description="สร้างเว็บไซต์ให้บริษัททดสอบ 1 ของคุณ"
+        work_type="Fulltime"
+        workplace="Hybrid"
+        career_stage="Entry-Level"
+        province="กรุงเทพมหานคร"
+        country="ประเทศไทย"
+        salary="30,000"
+        imgUrl="https://drive.google.com/uc?export=view&id=1bsT5WNkFnhhGT7SD3AynO9gqDjzz17lc"
+        updatedDate="2024-12-29:10:00"
+        industry={["IT", "เทคโนโลยี", "องค์กร", "งานทดสอบ"]}
       />
     );
     const element = screen.getByRole("img");
@@ -44,22 +68,30 @@ describe("JobCard", () => {
   test("check types of props", () => {
     render(
       <JobCard
-        orgName="องค์กรทดสอบ 4"
-        imgUrl="https://drive.google.com/uc?export=view&id=1D9ldIaOqNZVaGemuZiKPbHHZOgAv46S9"
-        jobTitle="งานทดสอบ 4"
-        location="ออนไลน์"
+        orgName="องค์กรทดสอบ 1"
+        title="พนักงานทดสอบ 1"
+        description="สร้างเว็บไซต์ให้บริษัททดสอบ 1 ของคุณ"
+        work_type="Fulltime"
+        workplace="Hybrid"
+        career_stage="Entry-Level"
+        province="กรุงเทพมหานคร"
+        country="ประเทศไทย"
+        salary="30,000"
+        imgUrl="https://drive.google.com/uc?export=view&id=1bsT5WNkFnhhGT7SD3AynO9gqDjzz17lc"
+        updatedDate="2024-12-29:10:00"
+        industry={["IT", "เทคโนโลยี", "องค์กร", "งานทดสอบ"]}
       />
     );
 
-    const element1 = screen.getByText(/องค์กรทดสอบ 4/i);
+    const element1 = screen.getByText(/องค์กรทดสอบ 1/i);
     const element2 = screen.getByRole("img");
-    const element3 = screen.getByText(/งานทดสอบ 4/i);
-    const element4 = screen.getByText(/ออนไลน์/i);
+    const element3 = screen.getByText(/งานทดสอบ 1/i);
+    const element4 = screen.getByText(/Hybrid/i);
 
-    expect(element1).toHaveTextContent("องค์กรทดสอบ 4");
+    expect(element1).toHaveTextContent("องค์กรทดสอบ 1");
     expect(element2).toHaveAttribute("src");
     expect(element2).toHaveAttribute("alt");
-    expect(element3).toHaveTextContent("งานทดสอบ 4");
-    expect(element4).toHaveTextContent("ออนไลน์");
+    expect(element3).toHaveTextContent("พนักงานทดสอบ 1");
+    expect(element4).toHaveTextContent("Hybrid");
   });
 });
